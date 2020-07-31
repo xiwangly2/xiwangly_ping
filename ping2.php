c3f149cff5a22c7877118b1f446ab6524d08879d
LY <1334850101@qq.com>
2020-07-31 09:07:33 +0800

    Update ping2.php

--- ping2.php
+++ ping2.php
@@ -1,10 +1,17 @@
 <?php
 //这个文件是父文件。
-//父文件必须放在可执行危险命令的Windows服务器中，你可以借用现成的父文件，子文件只要可执行php的设备就行了（php版本推荐>7.0）可以父、子文件放在同一设备。
+//父文件必须放在可执行危险命令的服务器（Windows或Linux）中，你可以借用现成的父文件，子文件只要可执行php的设备就行了（php版本推荐>7.0）可以父、子文件放在同一设备。
 $ip = $_GET["url"];
 header("content-type:text/html;charset=GBK");
 $info = array();
-$sc = system("ping $ip",$info);
+if(substr(php_uname("s"),0,3) == "Win")
+{
+	$sc = system("ping $ip",$info);
+}
+else
+{
+	$sc = system("ping -c 3 $ip",$info);
+}
 //header("content-type:text/html;charset=UTF-8");
 //echo iconv("GBK","UTF-8",$sc);
 echo $sc;
